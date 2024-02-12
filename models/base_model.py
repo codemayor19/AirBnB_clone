eddad83e248ff0dcce719e91e393608a75d196b8
codemayor19 <codemayor19@gmail.com>
2024-02-12 02:48:26 -0800

    new commit

--- models/base_model.py
+++ models/base_model.py
@@ -8,19 +8,35 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """initialize an instance of the basemodel"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def to_dict(self):
+        """to_dict"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+
         return obj_dict
 
     def save(self):
+        """save"""
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """str"""
         class_name = self.__class__.__name__
+
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
